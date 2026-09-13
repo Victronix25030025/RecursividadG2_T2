a5b0ce4be42f4f24e4b6954edaf8d067c6009807
victor palacios <25030025@itesa.edu.mx>
2026-09-12 19:04:45 -0500

    Factorial recursivo completado

---
+++ Recursibilidad.U.I.Consola/Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalcularFactorial(int num)
+        {
+            //Caso base o condicion de salida
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            //Caso general
+            return num * CalcularFactorial(num - 1);
+        }
+    }
+}
